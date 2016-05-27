e4dda2a8b4ba55097bbf9a4e53020d678b71b216
Marc Neuhaus <marc@mia3.com>
2016-05-27 11:48:06 +0200

    improved WrapWordsViewHelper

--- Classes/ViewHelpers/WrapWordsViewHelper.php
+++ Classes/ViewHelpers/WrapWordsViewHelper.php
@@ -16,18 +16,33 @@ namespace MIA3\Mia3Search\ViewHelpers;
 
 /**
  */
-class WrapWordsViewHelper extends \TYPO3\CMS\Fluid\Core\ViewHelper\AbstractViewHelper {
+class WrapWordsViewHelper extends \TYPO3\CMS\Fluid\Core\ViewHelper\AbstractViewHelper
+{
     /**
      *
      * @param mixed $words
      * @param string $wrap
+     * @param integer $crop
+     * @param string suffix
+     * @param string $prefix
+     * @param string $wordsBeforeMatch
      * @return string
      */
-    public function render($words, $wrap = '<strong>|</strong>') {
+    public function render($words, $wrap = '<strong>|</strong>', $crop = null, $suffix = '&hellip;', $prefix = '&hellip;', $wordsBeforeMatch = 5)
+    {
         $content = $this->renderChildren();
         if (!is_array($words)) {
             $words = preg_split('/[ ,\.\?]/s', $words);
         }
+
+        if ($wordsBeforeMatch > 0) {
+//            $content = $this->cutBeforeMatch($content, $words, $crop, $prefix);
+        }
+
+        if ($crop !== NULL) {
+            $content = $this->cropWords($content, $crop, $suffix);
+        }
+
         foreach ($words as $word) {
             // do a case-insensitive search to find all case-sensitive matches
             preg_match_all('/(' . preg_quote($word) . ')/i', $content, $matches);
@@ -44,4 +59,41 @@ class WrapWordsViewHelper extends \TYPO3\CMS\Fluid\Core\ViewHelper\AbstractViewH
         return $content;
     }
 
+    public function cutBeforeMatch($content, $words, $crop, $prefix) {
+        $contentWords = preg_split('/[ ,\.\?]/s', $content);
+        foreach ($contentWords as $key => $contentWord) {
+            foreach ($words as $word) {
+                similar_text(strtolower($contentWord), strtolower($word), $match);
+                if ($match > 80) {
+                    $startPosition = strpos($content, $contentWord);
+                    for ($i = 1; $i <= $wordsBeforeMatch; $i++) {
+                        if (isset($contentWords[$key - $i])) {
+                            $startPosition -= strlen($contentWords[$key - $i]) + 1;
+                        }
+                    }
+//                    $content = $prefix . ' ' . substr($content, $startPosition);
+                    break;
+                }
+            }
+        }
+        return $content;
+    }
+
+    public function cropWords($text, $wordCount, $suffix = '&hellip;')
+    {
+        $text = strip_tags($text);
+
+        $words = preg_split("/[\n\r\t ]+/", $text, $wordCount + 1, PREG_SPLIT_NO_EMPTY);
+        $separator = ' ';
+
+        if (count($words) > $wordCount) {
+            array_pop($words);
+            $text = implode($separator, $words);
+            $text = $text . $suffix;
+        } else {
+            $text = implode($separator, $words);
+        }
+
+        return $text;
+    }
 }
